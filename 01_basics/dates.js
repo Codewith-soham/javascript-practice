350cc50ed426d0d963910f3b3506f40bab750526
Codewith-soham <ghadgesoham2006@gmail.com>
2026-08-18 00:36:11 +0530

    feat: add dates.js to demonstrate date properties and methods in JavaScript

---
+++ 01_basics/dates.js
@@ -0,0 +1,33 @@
+// Dates
+
+let myDate = new Date()
+
+//different types of date properties
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
+
+let myCreateDate = new Date(2023, 0, 23)  //In js months start with 0
+console.log(myCreateDate.toDateString());
+console.log(myCreateDate.toLocaleDateString());
+
+let newDate = new Date("01-14-2023")
+console.log(newDate.toString());
+
+let myTimeStamp = Date.now() //returns the current time
+
+console.log(myTimeStamp);
+console.log(myCreateDate.getTime());
+console.log(Math.floor(Date.now()/1000)); //to get second
+let newData = new Date()
+console.log(newData);
+console.log(newData.getMonth());
+console.log(newData.getDate());
+
+
+
+
+
+
+
