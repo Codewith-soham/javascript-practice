539a49e733c51ffaed9bacfbf26e6f809c0c2f36
Codewith-soham <ghadgesoham2006@gmail.com>
2026-08-25 11:45:43 +0530

    feat: add session management and JWT handling in server.js; update package.json and package-lock.json

--- exam_prep/express_practice/server.js
+++ exam_prep/express_practice/server.js
@@ -1,7 +1,17 @@
 const express = require("express")
-const cookie = require("cookie-parser")
+const cookie = require("cookie-parser") //small pieces of data that browser stores like login,shopping cart
+const session = require("express-session")
+const jwt = require('jsonwebtoken')
 
 const app = express()
+
+app.use(express.json()) //parses incoming json data into js objects
+
+app.use(session({
+    secret: "1234",
+    saveUninitialized:false,
+    resave:false
+}))
 
 app.get('/', (req,res) => {
     res.send("HELLO FROM EXPRESS")
@@ -24,6 +34,62 @@ app.get('/delete-cookie', (req,res) => {
     res.send("Cookie-cleared")
 })
 
+app.get('/create-session', (req,res) => {
+    req.session.username = "Soham",
+    res.send("Session created")
+})
+
+app.get('/get-session', (req,res) => {
+    res.send(req.session.username)
+})
+
+app.get('/delete-session', (req,res) => {
+    res.session.destroy((err) => {
+        if(err){
+            res.send("Cannot destroy sessions")
+        }
+        else{
+            res.send("Session destroyed")
+        }
+    })
+})
+
+//assign jwt
+app.use('/assign-token', (req,res) => {
+    const token = jwt.sign(
+        {
+            name: "Soham ghadge",
+            age: "18"
+        },
+        "1234"
+    )
+
+    res.send(token)
+})
+
+app.get("/verify-token", (req, res) => {
+    const token = req.query.token;
+
+    try {
+        const decoded = jwt.verify(token, "1234");
+
+        res.json(decoded);
+    } catch (error) {
+        res.send("Invalid token");
+    }
+});
+
+//read post req and respond
+app.post('/get-data', (req,res) => {
+    const data = req.body
+
+    res.json({
+        message: "Data recived succesfully",
+        revivedData: data 
+    })
+})
+
+//create server
 app.listen(3000, () => {
     console.log("Server running on port 3000");
     
