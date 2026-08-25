c66b33ef3c6326461053f2c78a0b416c48de0050
Codewith-soham <ghadgesoham2006@gmail.com>
2026-08-25 08:01:03 +0530

    feat: add initial buffer implementation with package and lock files

---
+++ exam_prep/buffer/bufferPrac.js
@@ -0,0 +1,9 @@
+//buffer is small memory used by node store binary data while it is being processed
+
+const buffer = Buffer.from("Hello soham")
+
+console.log(buffer);
+console.log(buffer.toString());
+
+
+
